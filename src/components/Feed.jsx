b9d8e9480e6b3b8051fce292780720025de85544
Mohammad Raza <raza.it7522@gmail.com>
2023-06-24 22:35:12 +0530

    initial commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { Sidebar, Videos } from "./";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { xs: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { xs: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { xs: 0, md: 2 },
+          backgroundColor: "black",
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "black" }}
+        >
+          Copyright 2022 RAZA Media
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          m={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
